33447a4ae3fd44c4dc3bfd9bbf6243df2fbb09a5
Johandika <johanelyosse@gmail.com>
2023-03-08 13:16:10 +0700

    Talent API

--- app/api/v1/images/controller.js
+++ app/api/v1/images/controller.js
@@ -3,7 +3,7 @@ const { createImages } = require("../../../services/mongoose/images");
 
 const create = async (req, res, next) => {
   try {
-    const result = await createImages;
+    const result = await createImages(req);
     res.status(StatusCodes.CREATED).json({ data: result });
   } catch (error) {
     next(error);
